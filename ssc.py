cbbe5826bde31d1ae4c5981aa4768ee15a66b632
Ari <anisfeld@uchicago.edu>
2017-09-05 19:29:08 -0500

    scopeathon the thrid

--- ssc.py
+++ ssc.py
@@ -2,8 +2,10 @@ from flask import Flask, render_template, request
 from datetime import datetime
 try: #failing for Vi
 	from flaskext.markdown import Markdown
-except:
-	from markdown import Markdown
+except: 
+	# Ari tried this; it did not seem compatible with Flask objects 
+	# He got the 'try' to work
+	from markdown import Markdown 
 import os
 
 app = Flask(__name__)
@@ -11,7 +13,7 @@ app.config['DEBUG'] = True
 
 Markdown(app)
 
-path = 'static/events'
+PATH = 'static/events'
 
 #creates list of upcoming and past events
 def get_events_lists():
@@ -19,25 +21,28 @@ def get_events_lists():
 	past = []
 	present = datetime.now().date()
 
-	for f in os.listdir(path):
-		year, mon, day = int(f[0:4]), int(f[5:7]), int(f[8:10])
-		if datetime.date(datetime(year,mon,day))<present:
-			event_dict = file_to_dict(f)
-			event_dict['filename'] = f
-			past.append(event_dict)
-		else:
-			event_dict = file_to_dict(f)
-			event_dict['filename'] = f
-			upcoming.append(event_dict)
-		upcoming = sorted(upcoming, key=lambda event: event['filename'])
-		past = sorted(past, key=lambda event: event['filename'])
+	for f in os.listdir(PATH):
+		try:
+			year, mon, day = int(f[0:4]), int(f[5:7]), int(f[8:10])
+			if datetime.date(datetime(year,mon,day)) < present:
+				event_dict = file_to_dict(f)
+				event_dict['filename'] = f
+				past.append(event_dict)
+			else:
+				event_dict = file_to_dict(f)
+				event_dict['filename'] = f
+				upcoming.append(event_dict)
+			upcoming = sorted(upcoming, key=lambda event: event['filename'])
+			past = sorted(past, key=lambda event: event['filename'])
+		except:
+			print('%s does not match event file naming convention' % f)
 	return upcoming, past
 
 
 #makes event.txt into dictionary
 def file_to_dict(filename):
 	file_dict = {}
-	with open(os.path.join(path, filename)) as f:
+	with open(os.path.join(PATH, filename)) as f:
 		content = f.read().split('|')
 		if len(content) < 4:
 			file_dict['title']=content[0]
@@ -64,7 +69,8 @@ def scopeathon():
 	upcoming, past = get_events_lists()
 	past.reverse()
 	#return render_template('hackathon.html', hack_flag = True)
-	return render_template('2016hackathon.html', past=past, hack_flag = True)
+	#return render_template('2016hackathon.html', past=past, hack_flag = True)
+	return render_template('2017scopeathon.html', past=past, hack_flag = True)
 
 @app.route('/past_events')
 def past_events():
